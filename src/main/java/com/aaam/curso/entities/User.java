3074360dddb52e6c43057a65d7883885c4ccb056
AAAM <aaam@gmail.com>
2026-04-03 23:41:59 +0200

    H2 database, test profile, JPA

--- src/main/java/com/aaam/curso/entities/User.java
+++ src/main/java/com/aaam/curso/entities/User.java
@@ -3,9 +3,19 @@ package com.aaam.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -16,7 +26,6 @@ public class User implements Serializable {
 	
 	}
 
-
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -25,26 +34,18 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-
- 
 		
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
-
 	public String getName() {
 		return name;
 	}
-
-
 
 	public void setName(String name) {
 		this.name = name;
