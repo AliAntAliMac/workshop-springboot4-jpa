fce81170efedcea2d5b5eb4f7f768e3b3bf85f23
AAAM <aaam@gmail.com>
2026-04-04 20:35:41 +0200

    Product

---
+++ src/main/java/com/aaam/curso/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.aaam.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aaam.curso.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Long>{
+
+}
